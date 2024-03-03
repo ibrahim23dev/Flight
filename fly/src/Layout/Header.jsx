f90068fa1100e8e097388d1d5c2ab1ad3972dc72
Ibrahim <ibrahim.soft.dev23@gmail.com>
2024-03-04 01:23:56 +0600

    ok
    let's go

--- fly/src/Layout/Header.jsx
+++ fly/src/Layout/Header.jsx
@@ -1,36 +1,29 @@
-import React from 'react'
-import '../Utils/Navbar.css';
-import logowhite from "../assets/logo.png";
-import logoblack from "../assets/adbop.png";
-import searchb from "../assets/search-b.png";
-import searchw from "../assets/search-w.png";
-import day from "../assets/day.png";
-import night from "../assets/night.png";
-function Header({ theme, SetTheme }) {
-          const toggle_mode = () => {
-        // Use === for comparison in the ternary operator
-        theme === 'light' ? SetTheme('dark') : SetTheme('light');
-    }
+import React from "react";
+import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrate } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
+function Header() {
   return (
-     <div className='navbar'>
-            
-            <img src={theme === 'light' ? logoblack : logowhite} alt="logo" className='logo' />
-            <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/'>Flight</a></li>
-                <li><a href='/'>Hotel</a></li>
-                                <li><a href='/'>Event</a></li>
-                                <li><a href='/'>Contac Us</a></li>
-            </ul>
-            <div className='search-box'>
-                <input type='text' placeholder='search' className='input' />
-                {/* Alt attribute for accessibility */}
-                <img src={theme === 'light' ? searchw : searchb} alt="search" />
-            </div>
-            {/* Alt attribute for accessibility */}
-            <img onClick={toggle_mode} src={theme === 'light' ? night : day} alt="toggle" className='toggle' />
-        </div>
-  )
+    <nav className="flex">
+      <div className="flex">
+        <SiConsul />
+      </div>
+      <div className="flex visible">
+        <li className="flex">
+          <BsPhoneVibrate />
+          Support
+        </li>
+        <li className="flex">
+          <AiOutlineGlobal />
+          language
+        </li>
+      </div>
+      <div>
+        <span>Sing In</span>
+        <span>Sing Out</span>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
